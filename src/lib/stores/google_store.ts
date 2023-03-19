ee78bcc3517ace76060cb15b02eef40ed07566df
Colby McHenry <me@colbymchenry.com>
2023-03-19 16:52:55 -0400

    d

--- src/lib/stores/google_store.ts
+++ src/lib/stores/google_store.ts
@@ -1,8 +1,13 @@
+import { browser } from "$app/environment";
 import { GoogleAPI, type IGoogleAPI } from "$lib/services/google";
 import { writable, type Writable } from "svelte/store";
 
 export const googleApiKey = "AIzaSyBmoM2S5uCZve7XvVWnlvhWMH9IPjRhtfM";
 
-export const googleStore: Writable<IGoogleAPI> = writable<IGoogleAPI>(typeof window !== 'undefined' ? new GoogleAPI(googleApiKey) : undefined);
+export const googleStore: Writable<IGoogleAPI> = writable<IGoogleAPI>(undefined);
+
+if (browser) {
+    googleStore.set(new GoogleAPI(googleApiKey));
+}
 
 export const googleHomeMap = writable(undefined);
